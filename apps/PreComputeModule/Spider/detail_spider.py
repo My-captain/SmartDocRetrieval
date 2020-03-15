e780590f0e5889aa0b03c65c33e55f72c911c2cc
Mr.Robot <1028813178@qq.com>
2020-03-15 23:07:54 +0800

    增加selenium抓取文献的脚本
    doc_detail1.json中为2k个详情url抓取结果

--- apps/PreComputeModule/Spider/detail_spider.py
+++ apps/PreComputeModule/Spider/detail_spider.py
@@ -20,37 +20,45 @@ def get_empty_doc():
     }
 
 
-def save_hrefs(href_list):
-    with open(r"./detail_href_list_sorted_by_latest.json", "w", encoding="utf-8") as file:
+def save_detail(href_list):
+    with open(r"./doc_detail.json", "w", encoding="utf-8") as file:
         file.write(json.dumps(href_list, ensure_ascii=False))
 
 
-driver = webdriver.Chrome(executable_path="./chromedriver.exe")
+driver = webdriver.Chrome('./chromedriver')
 
 href_list = None
-with open(r"./detail_href_list_sorted_by_latest.json", "w", encoding="utf-8") as file:
-    href_list = json.load(file.read())
+with open(r"./detail_href_list_sorted_by_latest.json", "r", encoding="utf-8") as file:
+    file_content = file.read()
+    href_list = json.loads(file_content)
 
+detail_list = list()
 
-for url in href_list:
-    driver.get(url)
-    title = driver.find_element_by_class_name("citation__title").text()
-    authors = list()
-    author_card = driver.find_element_by_id("sb-1")
-    for i in author_card.find_elements_by_class_name("loa__item"):
-        authors.append(i.text())
-    publication = driver.find_element_by_class_name("epub-section__title").text()
-    abstract = driver.find_element_by_class_name("hlFld-Abstract").text()
-    references = list()
-    refers = driver.find_elements_by_class_name("references__item")
-    for refer in refers:
-        references.append(refer.text())
+for url in href_list[1646:]:
+    while True:
+        try:
+            driver.get(url)
+            title = driver.find_element_by_class_name("citation__title").text
+            authors = list()
+            author_card = driver.find_element_by_id("sb-1")
+            for i in author_card.find_elements_by_class_name("loa__item"):
+                authors.append(i.text)
+            publication = driver.find_element_by_class_name("epub-section__title").text
+            abstract = driver.find_element_by_class_name("hlFld-Abstract").text
+            references = list()
+            refers = driver.find_elements_by_class_name("references__item")
+            for refer in refers:
+                references.append(refer.text)
+            break
+        except Exception as e:
+            print("出现异常")
+            time.sleep(0.8)
     doc = get_empty_doc()
     doc["title"] = title
     doc["authors"] = authors
     doc["publication"] = publication
     doc["abstract"] = abstract
     doc["references"] = references
-    next_page = driver.find_element_by_css_selector("a.pagination__btn--next")
-    next_page.click()
-    time.sleep(10)
+    detail_list.append(doc)
+    save_detail(detail_list)
+    time.sleep(1)
